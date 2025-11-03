9b506e3b845567c7b476cf3d86a6a9459d4c5a17
testartemis <164508523+zArtemis0@users.noreply.github.com>
2025-11-03 11:50:39 -0500

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+// pages/index.tsx
+import React from 'react';
+
+const CanvasPage = () => {
+  return (
+    <div>
+      <h1>r-place Clone (1000x1000)</h1>
+      <p>This is where the canvas will be rendered.</p>
+      {/* The actual <canvas> element and logic will go here later */}
+    </div>
+  );
+};
+
+export default CanvasPage;
