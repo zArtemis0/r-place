b008ab024ec06a1e59fa914cb2c1a53634b06ee5
testartemis <164508523+zArtemis0@users.noreply.github.com>
2025-11-03 10:19:07 -0500

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,1 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+import Sketch from 'react-sketch';
 
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_IO_URL);
+
+const Canvas = () => {
+  const sketchRef = useRef(null);
+
+  useEffect(() => {
+    socket.on('initialCanvas', (data) => {
+      sketchRef.current.loadCanvas(data);
+    });
+
+    socket.on('draw', (data) => {
+      sketchRef.current.loadCanvas(data);
+    });
+  }, []);
+
+  const handleDraw = (data) => {
+    socket.emit('draw', data);
+  };
+
+  return (
+    <Sketch
+      ref={sketchRef}
+      onDraw={handleDraw}
+      width={1000}
+      height={1000}
+    />
+  );
+};
+
+export default Canvas;
